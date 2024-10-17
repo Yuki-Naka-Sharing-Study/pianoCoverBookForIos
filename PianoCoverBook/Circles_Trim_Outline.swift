9ab2763c47cecc0a7c8b5ad6b4f806c82eb60953
Yuki Naka <yukinaka2168@au.com>
2024-10-17 10:36:26 +0900

    「カバーしている曲」と「右手のカバー度」と「左手のカバー度」

---
+++ PianoCoverBook/Circles_Trim_Outline.swift
@@ -0,0 +1,18 @@
+//
+//  Circles_Trim_Outline.swift
+//  PianoCoverBook
+//
+//  Created by 仲優樹 on 2024/10/17.
+//
+
+import SwiftUI
+
+struct Circles_Trim_Outline: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Circles_Trim_Outline()
+}
