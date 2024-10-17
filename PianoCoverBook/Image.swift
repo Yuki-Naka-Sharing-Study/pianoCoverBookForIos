68239845eb7d437496e1e4de9ab6a46a42b3566e
Yuki Naka <yukinaka2168@au.com>
2024-10-17 15:47:59 +0900

    「SettingView」と「Image」を追加。

---
+++ PianoCoverBook/Image.swift
@@ -0,0 +1,18 @@
+//
+//  Image.swift
+//  PianoCoverBook
+//
+//  Created by 仲優樹 on 2024/10/17.
+//
+
+import SwiftUI
+
+struct Image: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Image()
+}
---
+++ PianoCoverBook/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  PianoCoverBook
+//
+//  Created by 仲優樹 on 2024/10/17.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
