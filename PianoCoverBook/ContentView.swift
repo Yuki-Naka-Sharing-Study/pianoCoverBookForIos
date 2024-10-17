08ec333df10f8a9a911cb97f33986cf45d6ad396
Yuki Naka <yukinaka2168@au.com>
2024-10-17 10:23:09 +0900

    Initial Commit

---
+++ PianoCoverBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PianoCoverBook
+//
+//  Created by 仲優樹 on 2024/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PianoCoverBook/PianoCoverBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoCoverBookApp.swift
+//  PianoCoverBook
+//
+//  Created by 仲優樹 on 2024/10/17.
+//
+
+import SwiftUI
+
+@main
+struct PianoCoverBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
