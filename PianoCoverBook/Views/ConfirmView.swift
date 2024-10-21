2844a1c4046024716fcb608d72c5c2b690106bf6
Yuki Naka <yukinaka2168@au.com>
2024-10-21 09:57:20 +0900

    「ConfirmView (記録画面)」に「.searchable」含め検索UIとその関連コードを追加。

--- PianoCoverBook/Views/ConfirmView.swift
+++ PianoCoverBook/Views/ConfirmView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct ConfirmView: View {
+    private let nameOfArtists = ["Bach", "Beethoven", "Chopin"]
+    @State private var searchResults: [String] = []
+    @State private var nameOfArtistsSearch = ""
+    @State private var nameOfArtist = ""
+    
     var body: some View {
-        Text("まだ記録がありません。\nピアノを練習して記録を追加しましょう。")
-            .multilineTextAlignment(.center)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationStack {
+            VStack(spacing: 20.0) {
+                Text("まだ何も記録されていません。")
+                Text(nameOfArtist)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .font(.title)
+            .navigationTitle(Text("記録"))
+        }
+        .searchable(
+            text: $nameOfArtistsSearch,
+            placement: .navigationBarDrawer(displayMode: .always),
+            prompt: Text("アーティスト名・曲名で検索")
+        ) {
+            ForEach(searchResults, id: \.self) { name in
+                Button(name) {
+                    nameOfArtist = name
+                }
+            }
+        }
+        .onChange(of: nameOfArtistsSearch) { _, nameOfArtist in
+            searchResults = nameOfArtists.filter { name in
+                name.hasPrefix(nameOfArtistsSearch)
+            }
+        }
     }
 }
 
