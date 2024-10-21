d168cdb8a38ede6f722780926238ec0e9b33f2b6
Yuki Naka <yukinaka2168@au.com>
2024-10-21 15:03:30 +0900

    「Object」を継承した「Music」クラスを作成。

---
+++ PianoCoverBook/Models/Music.swift
@@ -0,0 +1,18 @@
+//
+//  Music.swift
+//  PianoCoverBook
+//
+//  Created by 仲優樹 on 2024/10/21.
+//
+
+import SwiftUI
+
+struct Music: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Music()
+}
