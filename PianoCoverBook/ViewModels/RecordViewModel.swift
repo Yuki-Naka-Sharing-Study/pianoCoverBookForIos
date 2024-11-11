77c7d274a14cd041f3e1bd86e420a49dc9cb6a9d
仲優樹 <yu-ki216m@outlook.jp>
2024-11-11 15:15:55 +0900

    deleted navigationTitle, inputNameText and isNameEmpty.

--- PianoCoverBook/ViewModels/RecordViewModel.swift
+++ PianoCoverBook/ViewModels/RecordViewModel.swift
@@ -11,38 +11,19 @@ import RealmSwift
 class RecordViewModel: ObservableObject {
     @Published private var model = RecordModel()
     
-    // 以下、必要なコード
-    var navigationTitle: String {
-        return "空文字を判定"
-    }
-    
-    // 以下、必要なコード
-    var inputNameText: String {
-        get { model.karaMojiNameText }
-        set { model.karaMojiNameText = newValue }
-    }
-    
-    // $musicName
     var musicName: String {
         get { model.musicNameText }
         set { model.musicNameText = newValue }
     }
     
-    // $artistName
     var artistName: String {
         get { model.artistNameNameText }
         set { model.artistNameNameText = newValue }
     }
     
-    // $memo
     var memo: String {
         get { model.memoText }
         set { model.memoText = newValue }
-    }
-    
-    // 以下、必要なコード
-    var isNameEmpty: Bool {
-        inputNameText.trimmingCharacters(in: .whitespaces).isEmpty
     }
     
     var isMusicNameEmpty: Bool {
